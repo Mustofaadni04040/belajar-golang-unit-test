36a847bc5f780f83138c92d21d5d19560a5c5208
mustofa adny <adniadni04@gmail.com>
2025-06-07 15:35:14 +0700

    fail unit test using Error & Fatal

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -24,3 +24,22 @@ func TestHelloWorld2(t *testing.T) {
 
 	fmt.Println("TestHelloWorld2 done")
 }
+func TestHelloWorld3(t *testing.T) {
+	result := HelloWorld("Siti Anjani")
+
+	if result != "Hello Siti Anjani" {
+		t.Error("Result must be Hello Siti Anjani") // tetap menjalankan kode di bawahnya
+	}
+
+	fmt.Println("TestHelloWorld3 done")
+}
+
+func TestHelloWorld4(t *testing.T) {
+	result := HelloWorld("Asep Anjani")
+
+	if result != "Hello Asep Anjani" {
+		t.Fatal("Result must be Hello Asep Anjani") // tidak menjalankan kode di bawahnya
+	}
+
+	fmt.Println("TestHelloWorld4 done")
+}
