52d98dff918619098ad10ede05719b574844c15b
mustofa adny <adniadni04@gmail.com>
2025-06-07 15:29:01 +0700

    fail unit test using Fail & FailNow

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,11 +1,26 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("Mustofa Adny")
 
 	if result != "Hello Mustofa Adny" {
-		panic("Result is not Hello Mustofa Adny")
+		t.Fail() // tetap menjalankan kode di bawahnya
 	}
+
+	fmt.Println("TestHelloWorld done")
+}
+
+func TestHelloWorld2(t *testing.T) {
+	result := HelloWorld("Ucok Udin")
+
+	if result != "Hello Ucok Udin" {
+		t.FailNow() // tidak menjalankan kode di bawahnya
+	}
+
+	fmt.Println("TestHelloWorld2 done")
 }
